6810e81ab04787d6df9f67a9225c756360453158
Alyona <polova_alyona0907@ukr.net>
2023-06-25 20:44:51 +0200

    task 3 done

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,37 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// for (let i = 0; i < images.length; i += 1) {
+//   const image = images[i];
+
+//   const liEl = document.createElement("li");
+//   const imgEl = document.createElement("img");
+//   imgEl.src = image.url;
+//   imgEl.alt = image.alt;
+//   imgEl.width = 500;
+//   liEl.appendChild(imgEl);
+
+//   const itemEl = document.querySelector(".gallery");
+//   itemEl.appendChild(liEl);
+// }
+
+const itemEl = document.querySelector(".gallery");
+let liEl = "";
+for (let i = 0; i < images.length; i += 1) {
+  const image = images[i];
+  liEl += `<li><img src = "${image.url}" alt = "${image.alt}" width = "500" ></li>`;
+}
+
+itemEl.insertAdjacentHTML("afterbegin", liEl);
