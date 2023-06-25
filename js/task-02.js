35805654e6a32d501dace9a23860c941a4cd2a34
Alyona <polova_alyona0907@ukr.net>
2023-06-25 16:32:44 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const newLiEl = document.createElement("li");
+  newLiEl.classList.add("item");
+  newLiEl.textContent = ingredients[i];
+  const navEl = document.querySelector("#ingredients");
+  navEl.appendChild(newLiEl);
+}
